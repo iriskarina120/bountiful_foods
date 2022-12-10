328fb154f3193a7c93799c977386a091408f8365
iriskarina <iriskarina120120@gmail.com>
2022-12-10 19:25:03 -0300

    six changes

--- js/drink.js
+++ js/drink.js
@@ -1,20 +1,38 @@
-const myfruits = {};
-const output = (myfruits) => {
-    myfruits.forEach((fruit) => {
+const myFruits = {};
+const output = (myFruits) => {
+    myFruits.forEach((fruit) => {
       let article = document.createElement("article");
-        let name = document.createElement("h3");
-        name.textContent = fruit.name; 
-        
-        
-        article.appendChild(name);
+        let fruitName = document.createElement("h3");
+        fruitName.textContent = fruit.name;
+        let carbohydrates = document.createElement("ul");        
+        carbohydrates.textContent = `Carbohydrates: ${fruit.carbohydrates}`;        
 
-        document.querySelector("#curses").appendChild(article);
+        let protein = document.createElement("ul");        
+        protein.textContent = `Protein: ${fruit.protein}`;       
+
+        let fat = document.createElement("ul");        
+        fat.textContent = `Fat: ${fruit.fat}`;        
+
+        let calories = document.createElement("ul");        
+        calories.textContent = `Calories: ${fruit.calories}`;        
+
+        let sugar = document.createElement("ul");        
+        sugar.textContent = `Sugar: ${fruit.sugar}`;              
+        
+        article.appendChild(fruitName);
+        article.appendChild(carbohydrates); 
+        article.appendChild(protein);       
+        article.appendChild(fat);
+        article.appendChild(calories);
+        article.appendChild(sugar);        
+            
+        document.querySelector("#myFruits").appendChild(article);
     });
     };
 
 const getfruits = async () => {
     const response = await fetch(
-      "https://brotherblazzard.github.io/canvas-content/fruit.json"
+      "https://iriskarina120.github.io/bountiful-json/bountiful-json/bountiful.json"
     );
     fruitList = await response.json();
     output(fruitList);
@@ -22,32 +40,7 @@ const getfruits = async () => {
   getfruits();
 
 const reset = () => {
-    document.querySelector("#curses").innerHTML = "";
+    document.querySelector("#myFruits").innerHTML = "";
   };
 
-const sortBy = () => {
-    reset();
-  
-    let filter = document.querySelector("#sortBy").value;
-  
-    switch (filter) {
-      case "fruit1":
-        output(fruitList);
-        break;
-      case "fruit":        
-        break;
-      default:        
-        output(
-          fruitList.sort((curse1, curse2) =>
-            curse1.name > curse2.name
-              ? 1
-              : curse2.name >
-                curse1.name
-              ? -1
-              : 0
-          )
-        );
-        break;
-    }
-  };
 document.querySelector("#sortBy").addEventListener("change", sortBy);
--- js/menu-jam.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburguerBtn").classList.toggle("open");
-}
-const x = document.getElementById("hamburguerBtn");
-x.onclick = toggleMenu;
